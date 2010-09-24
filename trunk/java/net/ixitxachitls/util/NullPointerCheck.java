82ca62bb8102310d7850af264b1a63adb4254e61
balsiger@ixitxachitls.net <balsiger@ixitxachitls.net@b9f0664d-d9d2-19a3-23b2-6533f2ddd7f2>
2010-09-24 16:57:20 +0000

    Initial

---
+++ trunk/java/net/ixitxachitls/util/NullPointerCheck.java
@@ -0,0 +1,301 @@
+/******************************************************************************
+ * Copyright (c) 2002,2003 Peter 'Merlin' Balsiger and Fredy 'Mythos' Dobler
+ * All rights reserved
+ *
+ * This file is part of Dungeon Master Assistant.
+ *
+ * Dungeon Master Assistant is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Dungeon Master Assistant is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Dungeon Master Assistant; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *****************************************************************************/
+
+//------------------------------------------------------------------ imports
+
+package net.ixitxachitls.util;
+
+import com.puppycrawl.tools.checkstyle.api.Check;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+//..........................................................................
+
+//------------------------------------------------------------------- header
+
+/**
+ * This is a special check for checkstyle to check if a method uses one of it's
+ * arguments without checking it for null (and throwing an
+ * IllegalArgumentException).
+ *
+ * @file          NullPointerCheck.java
+ * @author        balsiger@ixitxachitls.net (Peter Balsiger)
+ *
+ * @to_do         with a bit more understanding of the antlr library and tree
+ *                structure the method's speed could possibly be improved
+ *                considerably
+ *
+ */
+
+//..........................................................................
+
+//__________________________________________________________________________
+
+public class NullPointerCheck extends Check
+{
+  //--------------------------------------------------------- constructor(s)
+  //........................................................................
+
+  //-------------------------------------------------------------- variables
+  //........................................................................
+
+  //-------------------------------------------------------------- accessors
+
+  //--------------------------- getDefaultTokens ---------------------------
+
+  /**
+    * Get the tokens that should be considered for this check (METHOD_DEF).
+    *
+    * @return      an array with all the tokens to consider
+    *
+    */
+  public int[] getDefaultTokens()
+  {
+    return new int [] { TokenTypes.METHOD_DEF, };
+  }
+
+  //........................................................................
+
+  //........................................................................
+
+  //----------------------------------------------------------- manipulators
+  //........................................................................
+
+  //------------------------------------------------- other member functions
+
+  //------------------------------ visitToken ------------------------------
+
+  /**
+    * Visit the token from the walker to make the checks.
+    *
+    * @param       inAST the node in the tree to check
+    *
+    */
+  public void visitToken(DetailAST inAST)
+  {
+    if(inAST == null)
+      throw new IllegalArgumentException("must have a node here");
+
+    // check only public methods
+    if(inAST.findFirstToken(TokenTypes.MODIFIERS) != null
+       && inAST.findFirstToken(TokenTypes.MODIFIERS)
+       .findFirstToken(TokenTypes.LITERAL_PUBLIC) != null)
+    {
+      // extract method name
+      String method = inAST.findFirstToken(TokenTypes.IDENT).getText();
+
+      // find parameters
+      DetailAST parameters = inAST.findFirstToken(TokenTypes.PARAMETERS);
+
+      // determine all the parameters used
+      for(DetailAST param =
+            parameters.findFirstToken(TokenTypes.PARAMETER_DEF);
+          param != null;
+          param = param.getNextSibling())
+      {
+        // ignore non parameters
+        if(param.getType() != TokenTypes.PARAMETER_DEF)
+          continue;
+
+        String parameter = param.findFirstToken(TokenTypes.IDENT).getText();
+
+        // check if we have a type that needs to be considered
+        if(param.findFirstToken(TokenTypes.TYPE) != null
+           && param.findFirstToken(TokenTypes.TYPE)
+           .findFirstToken(TokenTypes.IDENT) != null)
+        {
+          // see if this parameter is already checked for null
+          int check = findCheck(parameter, inAST);
+
+          // see if we find a method call for this parameter
+          int call = checkMethods(parameter, inAST);
+
+          if(call > 0 && (call < check || check == 0))
+          {
+            log(inAST.getLineNo(), "Method '" + method
+                + "()' does not check '" + parameter
+                + "' for null value but uses it on line " + call);
+          }
+        }
+      }
+    }
+  }
+
+  //........................................................................
+  //----------------------------- checkMethods -----------------------------
+
+  /**
+    * Check all the method call of the given node if they are called in an
+    * object variable with the given name.
+    * This actually determines if a method in the given object is called.
+    *
+    * @param       inParameter the name of the object to check for calls
+    * @param       inNode      the node to look in (including all children)
+    *
+    * @return      the line number of the line where a method call was found
+    *              or 0 if none was found
+    *
+    */
+  private int checkMethods(String inParameter, DetailAST inNode)
+  {
+    // already done?
+    if(inNode == null)
+      return 0;
+
+    // do we have a method call?
+    if(inNode.getType() == TokenTypes.METHOD_CALL)
+    {
+      DetailAST dot = inNode.findFirstToken(TokenTypes.DOT);
+
+      // ignore non object calls
+      if(dot != null)
+      {
+        // check if the name matches
+        if(dot.getFirstChild().getType() == TokenTypes.IDENT
+           && inParameter.equals(dot.getFirstChild().getText()))
+          return dot.getLineNo();
+      }
+    }
+
+    // do we have a variable use?
+    if(inNode.getType() == TokenTypes.DOT)
+    {
+      DetailAST first = inNode.findFirstToken(TokenTypes.IDENT);
+
+      // check if the name matches
+      if(first != null && inParameter.equals(first.getText()))
+        return first.getLineNo();
+    }
+
+    // check if we have other method calls
+    for(DetailAST node = inNode.getFirstChild();
+        node != null;
+        node = node.getNextSibling())
+    {
+      int line = checkMethods(inParameter, node);
+
+      if(line > 0)
+        return line;
+    }
+
+    // nothing found
+    return 0;
+  }
+
+  //........................................................................
+  //------------------------------ findCheck -------------------------------
+
+  /**
+    * Determine if in the given node there is a check for null of the named
+    * object. It is only checked if there is a null test, not what action is
+    * taken in such a case.
+    *
+    * @param       inParameter the parameter to check for
+    * @param       inNode      the node to look in
+    *
+    * @return      the line number where the check was found or 0 if none was
+    *              found
+    */
+  private int findCheck(String inParameter, DetailAST inNode)
+  {
+    // at the end?
+    if(inNode == null)
+      return 0;
+
+    // do we have an if?
+    if(inNode.getType() == TokenTypes.LITERAL_IF)
+    {
+      int line = findEquals(inParameter, inNode);
+
+      // found something?
+      if(line > 0)
+        return line;
+    }
+
+    // check if we have other checks
+    for(DetailAST node = inNode.getFirstChild();
+        node != null;
+        node = node.getNextSibling())
+    {
+      int line = findCheck(inParameter, node);
+
+      if(line > 0)
+        return line;
+    }
+
+    // nothing found
+    return 0;
+  }
+
+  //........................................................................
+  //------------------------------ findEquals ------------------------------
+
+  /**
+    * Find equal (and not equals) operators inside an if.
+    *
+    * @param       inParameter the parameter to look for
+    * @param       inNode      the node to look in
+    *
+    * @return      the line number where the equals operator with the name was
+    *              found, or 0 if none was found
+    *
+    */
+  private int findEquals(String inParameter, DetailAST inNode)
+  {
+    if(inNode == null)
+      return 0;
+
+    if(inNode.getType() == TokenTypes.EQUAL
+       || inNode.getType() == TokenTypes.NOT_EQUAL)
+    {
+      // check of parameter == null
+      if(inNode.getFirstChild().getType() == TokenTypes.IDENT
+         && inParameter.equals(inNode.getFirstChild().getText())
+         && inNode.getFirstChild().getNextSibling().getType()
+         == TokenTypes.LITERAL_NULL)
+        return inNode.getLineNo();
+    }
+
+    // check if we have other inNodeerators
+    for(DetailAST node = inNode.getFirstChild();
+        node != null;
+        node = node.getNextSibling())
+    {
+      int line = findEquals(inParameter, node);
+
+      if(line > 0)
+        return line;
+    }
+
+    // nothing found
+    return 0;
+  }
+
+  //........................................................................
+
+  //........................................................................
+
+  //------------------------------------------------------------------- test
+
+  // no tests here...
+
+  //........................................................................
+}
---
+++ trunk/java/net/ixitxachitls/util/test/TestCase.java
@@ -0,0 +1,183 @@
+/******************************************************************************
+ * Copyright (c) 2002,2003 Peter 'Merlin' Balsiger and Fredy 'Mythos' Dobler
+ * All rights reserved
+ *
+ * This file is part of Dungeon Master Assistant.
+ *
+ * Dungeon Master Assistant is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Dungeon Master Assistant is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Dungeon Master Assistant; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *****************************************************************************/
+
+//------------------------------------------------------------------ imports
+
+package net.ixitxachitls.util.test;
+
+//import net.ixitxachitls.util.logging.Log;
+
+//..........................................................................
+
+//------------------------------------------------------------------- header
+
+/**
+ * This is a simple enhancement to a JUnit TestCase, adding a mock logger
+ * to the test, including verifying test messages.
+ *
+ * @file          TestCase.java
+ *
+ * @author        balsiger@ixitxachitls.net (Peter 'Merlin' Balsiger)
+ *
+ */
+
+//..........................................................................
+
+//__________________________________________________________________________
+
+public class TestCase extends org.junit.Assert
+{
+  //--------------------------------------------------------- constructor(s)
+  //........................................................................
+
+  //-------------------------------------------------------------- variables
+
+  /** The mock logger to test logging statements. */
+  //  protected Log.Test.MockLogger m_logger;
+
+  /** The number of errors occurred. */
+  protected int m_errors = 0;
+
+  /** The number of failures occurred. */
+  protected int m_failures = 0;
+
+  static
+  {
+    // switch Configuration to test mode; must be done in a static init
+    // to make sure that all static inits in the test use the test mode
+    //net.ixitxachitls.util.configuration.Config.setRewriting(true);
+  }
+
+  /** The name of the test logger. */
+  private static final String s_logger = "test";
+
+  //........................................................................
+
+  //-------------------------------------------------------------- accessors
+
+  //----------------------------- assertEquals -----------------------------
+
+  /**
+   * Check that the given object when converted to String matches the given
+   * string.
+   *
+   * @param       inMessage  the message
+   * @param       inExpected the expected string
+   * @param       inActual   the actual object computed
+   *
+   */
+//   public void assertEquals(String inMessage, String inExpected,
+//   Object inActual)
+//   {
+//     assertEquals(inMessage, inExpected, inActual.toString());
+//   }
+
+  //........................................................................
+
+
+  //........................................................................
+
+  //----------------------------------------------------------- manipulators
+
+  //--------------------------------- run ----------------------------------
+
+  /**
+   * This method actually runs a test.
+   *
+   * @param       inResult the result of the test is returned with this
+   *
+   * @undefined   never
+   *
+   */
+  public void run(final junit.framework.TestResult inResult)
+  {
+    //super.run(inResult);
+
+    // call the special tearDown
+    try
+    {
+      tearDown(inResult.errorCount() > m_errors
+               || inResult.failureCount() > m_failures);
+
+      m_errors   = inResult.errorCount();
+      m_failures = inResult.failureCount();
+    }
+    catch(junit.framework.AssertionFailedError e)
+    {
+      //inResult.addError(this, e);
+    }
+  }
+
+
+
+  //........................................................................
+
+  //-------------------------------- setUp ---------------------------------
+
+  /**
+   * Setup the test for the next test case. This is called before each
+   * test method.
+   *
+   * @undefined   never
+   *
+   * @algorithm   just set up the mock logger to DEBUG level
+   *
+   * @derivation  possible, but call this one
+   *
+   */
+  public void setUp()
+  {
+    //    m_logger = new Log.Test.MockLogger();
+    //Log.add(s_logger, m_logger);
+    //Log.setLevel(Log.Type.DEBUG);
+    //net.ixitxachitls.util.configuration.Config.setRewriting(true);
+  }
+
+  //........................................................................
+  //------------------------------- tearDown -------------------------------
+
+  /**
+   * Tear down the test after a test case. This is called after each
+   * test method. This is a special, additional tearDown method with
+   * an additional method signaling that an error or failure occured.
+   *
+   * @param       inFailure - true if an error or failure occured, false
+   *                          if everything is ok
+   *
+   * @undefined   never
+   *
+   */
+  public void tearDown(final boolean inFailure)
+  {
+    //if(!inFailure)
+    //  m_logger.verify(getName());
+
+    //Log.remove(s_logger);
+    //m_logger = null;
+  }
+
+  //........................................................................
+
+  //........................................................................
+
+  //------------------------------------------------- other member functions
+  //........................................................................
+}
