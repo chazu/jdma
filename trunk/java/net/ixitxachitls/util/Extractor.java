63926da1c09bb5a8fbc1eb3a3d306d2ef73e4f42
balsiger@ixitxachitls.net <balsiger@ixitxachitls.net@b9f0664d-d9d2-19a3-23b2-6533f2ddd7f2>
2010-10-22 08:01:26 +0000

    added Extractor interface

---
+++ trunk/java/net/ixitxachitls/util/Extractor.java
@@ -0,0 +1,74 @@
+/******************************************************************************
+ * Copyright (c) 2002-2007 Peter 'Merlin' Balsiger and Fredy 'Mythos' Dobler
+ * All rights reserved
+ *
+ * This file is part of Dungeon Master Assistant.
+ *
+ * Dungeon Master Assistant is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Dungeon Master Assistant is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Dungeon Master Assistant; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *****************************************************************************/
+
+//------------------------------------------------------------------ imports
+
+package net.ixitxachitls.util;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+//..........................................................................
+
+//------------------------------------------------------------------- header
+
+/**
+ * An interface to extract data from an object.
+ *
+ * @file          Extractor.java
+ *
+ * @author        balsiger@ixitxachitls.net (Peter Balsiger)
+ *
+ * @param         <T> The type to extract from.
+ * @param         <V> The type extracted.
+ *
+ */
+
+//..........................................................................
+
+//__________________________________________________________________________
+
+public interface Extractor<T, V>
+{
+  //-------------------------------------------------------------- accessors
+
+  //--------------------------------- get ----------------------------------
+
+  /**
+   * Get the value from the given object.
+   *
+   * @param       inObject the object to get the data from
+   *
+   * @return      the extracted data
+   *
+   */
+  public @Nullable V get(@Nonnull T inObject);
+
+  //........................................................................
+
+  //........................................................................
+
+  //----------------------------------------------------------- manipulators
+  //........................................................................
+
+  //------------------------------------------------- other member functions
+  //........................................................................
+}
