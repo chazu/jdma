eb4ab4704bc19c75399e95834b170e6c3bc8436e
balsiger <balsiger@ixitxachitls.net>
2010-11-05 18:58:49 +0000

    added Indentificator

---
+++ java/net/ixitxachitls/util/Identificator.java
@@ -0,0 +1,72 @@
+/******************************************************************************
+ * Copyright (c) 2002,2003 Peter 'Merlin' Balsiger and Fredy 'Mythos' Dobler
+ * All rights reserved
+ *
+ * This file is part of Dungeon Master Assistant.
+ *
+ * Dungeon Master Assistant is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Dungeon Master Assistant is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Dungeon Master Assistant; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *****************************************************************************/
+
+//------------------------------------------------------------------ imports
+
+package net.ixitxachitls.util;
+
+import javax.annotation.Nonnull;
+
+//..........................................................................
+
+//------------------------------------------------------------------- header
+
+/**
+ * A interface defining a general identificator.
+ *
+ * @param         <T> The type to be identified.
+ *
+ * @file          Identificator.java
+ *
+ * @author        balsiger@ixitxachitls.net (Peter Balsiger)
+ *
+ */
+
+//..........................................................................
+
+//__________________________________________________________________________
+
+public interface Identificator<T>
+{
+  //-------------------------------------------------------------- accessors
+
+  //---------------------------------- id ----------------------------------
+
+  /**
+    * Determine the id(s) of the entry to identify it.
+    *
+    * @param       inEntry the entry to get the identification from
+    *
+    * @return      the list of ids
+    *
+    */
+  public @Nonnull String []id(@Nonnull T inEntry);
+
+  //........................................................................
+
+  //........................................................................
+
+  //------------------------------------------------------------------- test
+
+  // no test, interface only
+
+  //........................................................................
+}
