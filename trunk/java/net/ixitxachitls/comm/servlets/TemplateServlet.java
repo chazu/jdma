117a6293c83fc8b3f6124e18a018179bedfed963
balsiger@ixitxachitls.net <balsiger@ixitxachitls.net@b9f0664d-d9d2-19a3-23b2-6533f2ddd7f2>
2010-12-23 07:53:00 +0000

    added template servlet

---
+++ trunk/java/net/ixitxachitls/comm/servlets/TemplateServlet.java
@@ -0,0 +1,169 @@
+/******************************************************************************
+ * Copyright (c) 2002,2003 Peter 'Merlin' Balsiger and Fredy 'Mythos' Dobler
+ * All rights reserved
+ *
+ * This file is part of Dungeon Master Assistant.
+ *
+ * Dungeon Master Assistant is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Dungeon Master Assistant is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Dungeon Master Assistant; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *****************************************************************************/
+
+//------------------------------------------------------------------ imports
+
+package net.ixitxachitls.comm.servlets;
+
+import java.util.Date;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.ThreadSafe;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.easymock.EasyMock;
+
+import net.ixitxachitls.util.configuration.Config;
+import net.ixitxachitls.util.resources.Resource;
+import net.ixitxachitls.util.resources.TemplateResource;
+
+//..........................................................................
+
+//------------------------------------------------------------------- header
+
+/**
+ * This class represents a simple HTTP handler for static content. This can be
+ * html files, text or images.
+ *
+ * @file          FileServlet.java
+ *
+ * @author        balsiger@ixitxachitls.net (Peter Balsiger)
+ *
+ */
+
+//..........................................................................
+
+//__________________________________________________________________________
+
+@ThreadSafe
+public class TemplateServlet extends FileServlet
+{
+  //--------------------------------------------------------- constructor(s)
+
+  //--------------------------- TemplateServlet ----------------------------
+
+  /**
+   * Create the handler for templated files.
+   *
+   * @param       inRoot         the root directory (in classpath) for the
+   *                             files to be served
+   * @param       inType         the type of the files to handle (e.g. return)
+   * @param       inPrefix       the prefix into the configuration to get
+   *                             templates
+   *
+   */
+  public TemplateServlet(@Nonnull String inRoot, @Nullable String inType,
+                         @Nonnull String inPrefix)
+  {
+    super(inRoot, inType, false);
+
+    m_prefix = inPrefix;
+  }
+
+  //........................................................................
+
+  //........................................................................
+
+  //-------------------------------------------------------------- variables
+
+  /** The prefix into the configuration for template values. */
+  protected @Nonnull String m_prefix;
+
+  /** The id for serialization. */
+  private static final long serialVersionUID = 1L;
+
+  //........................................................................
+
+  //-------------------------------------------------------------- accessors
+
+  //----------------------------- getResource ------------------------------
+
+  /**
+   * Get the resource used for serving this request.
+   *
+   * @param       inPath the path to the resource
+   *
+   * @return      the requested resource
+   *
+   */
+  protected @Nonnull Resource getResource(String inPath)
+  {
+    return TemplateResource.get(inPath, m_prefix);
+  }
+
+  //........................................................................
+
+  //........................................................................
+
+  //----------------------------------------------------------- manipulators
+  //........................................................................
+
+  //------------------------------------------------- other member functions
+  //........................................................................
+
+  //------------------------------------------------------------------- test
+
+  /** The tests. */
+  public static class Test extends net.ixitxachitls.util.test.TestCase
+  {
+    //----- handle ---------------------------------------------------------
+
+    /**
+     * The handle Test.
+     *
+     * @throws Exception should not happen
+     */
+    @org.junit.Test
+    public void handle() throws Exception
+    {
+      Config.setRewriting(false);
+      HttpServletRequest request =
+        EasyMock.createMock(HttpServletRequest.class);
+      HttpServletResponse response =
+        EasyMock.createMock(HttpServletResponse.class);
+      BaseServlet.Test.MockServletOutputStream output =
+        new BaseServlet.Test.MockServletOutputStream();
+
+      EasyMock.expect(request.getPathInfo())
+        .andReturn("/config/test/test.template");
+      response.setHeader("Content-Type", "text/plain");
+      EasyMock.expect(response.getOutputStream()).andReturn(output);
+      EasyMock.replay(request, response);
+
+      TemplateServlet servlet =
+        new TemplateServlet("", "text/plain", "test/test/template");
+
+      assertNull("handle", servlet.handle(request, response));
+      assertEquals("content", "This is a single word template test.\n",
+                   output.toString());
+
+      output.close();
+      EasyMock.verify(request, response);
+      Config.setRewriting(true);
+    }
+
+    //......................................................................
+  }
+
+  //........................................................................
+}
---
+++ trunk/java/net/ixitxachitls/util/resources/TemplateResource.java
@@ -0,0 +1,217 @@
+/******************************************************************************
+ * Copyright (c) 2002-2007 Peter 'Merlin' Balsiger and Fredy 'Mythos' Dobler
+ * All rights reserved
+ *
+ * This file is part of Dungeon Master Assistant.
+ *
+ * Dungeon Master Assistant is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Dungeon Master Assistant is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Dungeon Master Assistant; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *****************************************************************************/
+
+//------------------------------------------------------------------ imports
+
+package net.ixitxachitls.util.resources;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.net.URL;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+
+import net.ixitxachitls.util.Strings;
+import net.ixitxachitls.util.configuration.Config;
+import net.ixitxachitls.util.logging.Log;
+
+//..........................................................................
+
+//------------------------------------------------------------------- header
+
+/**
+ * A resource for the file system with special variable translation.
+ *
+ * @file          TemplateResource.java
+ *
+ * @author        balsiger@ixitxachitls.net (Peter Balsiger)
+ *
+ */
+
+//..........................................................................
+
+//__________________________________________________________________________
+
+public class TemplateResource extends FileResource
+{
+  //--------------------------------------------------------- constructor(s)
+
+  //--------------------------- TemplateResource ----------------------------
+
+  /**
+   * Create the template file resource.
+   *
+   * @param    inName   the name of the file this resource represents
+   * @param    inURL    the url to the resource
+   * @param    inPrefix the prefix into the config for template values
+   *
+   */
+  TemplateResource(@Nonnull String inName, @Nullable URL inURL,
+                   @Nonnull String inPrefix)
+  {
+    super(inName, inURL);
+
+    m_prefix = inPrefix;
+  }
+
+  //........................................................................
+
+  //........................................................................
+
+  //-------------------------------------------------------------- variables
+
+  /** The prefix into the config for template values. */
+  private @Nonnull String m_prefix;
+
+  /** The contents of the file (caching). */
+  private @Nullable String m_content;
+
+  /** Flag if files should be reread each time they are accessed. */
+  private static boolean s_reread = Config.get("web/template.reread", true);
+
+  //........................................................................
+
+  //-------------------------------------------------------------- accessors
+
+  //--------------------------------- get ----------------------------------
+
+  /**
+   * Get the templated resource represented by the given name.
+   *
+   * @param       inName      the name of the resource, relative to the
+   *                          classpath
+   * @param       inPrefix    the prefix into the configuration for template
+   *                          values
+   *
+   * @return      the templated resource for this name
+   *
+   */
+  public static @Nonnull Resource get(@Nonnull String inName,
+                                      @Nonnull String inPrefix)
+  {
+    String name = inName;
+    if(!name.startsWith("/"))
+      name = "/" + name;
+
+    URL url =
+      Resource.class.getResource(net.ixitxachitls.util.Files.concatenate
+                                 ("/", name));
+
+    return new TemplateResource(name, url, inPrefix);
+  }
+
+  //........................................................................
+
+  //........................................................................
+
+  //----------------------------------------------------------- manipulators
+  //........................................................................
+
+  //------------------------------------------------- other member functions
+
+  //-------------------------------- write ---------------------------------
+
+  /**
+   * Write the resources to the given output.
+   *
+   * @param       inOutput the output stream to write to
+   *
+   * @return      true if writing ok, false if not
+   *
+   */
+  public boolean write(@Nonnull OutputStream inOutput)
+  {
+    if(m_content == null)
+    {
+      try
+      {
+        m_content = Files.toString(asFile(), Charsets.UTF_8);
+
+        // do the replacement for all variables
+        m_content = Strings.replaceTemplates(m_content, m_prefix);
+      }
+      catch(java.io.IOException e)
+      {
+        Log.warning("cannot read resource '" + this + ": " + e);
+        return false;
+      }
+    }
+
+    try
+    {
+      inOutput.write(m_content.getBytes());
+    }
+    catch(java.io.IOException e)
+    {
+      return false;
+    }
+
+    if(s_reread)
+      m_content = null;
+
+    return true;
+  }
+
+  //........................................................................
+
+  //........................................................................
+
+  //------------------------------------------------------------------- test
+
+  /** The test. */
+  public static class Test extends net.ixitxachitls.util.test.TestCase
+  {
+    //----- write ----------------------------------------------------------
+
+    /** The write Test. */
+    @org.junit.Test
+    public void write()
+    {
+      Config.setRewriting(false);
+      Resource resource =
+        new TemplateResource("/config/test/test.config",
+                             TemplateResource.class
+                             .getResource("/config/test/test.config"),
+                             "test/test/template");
+
+      ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+      assertTrue("writing", resource.write(output));
+      assertPattern("content", ".*test.templateresource=single word.*",
+                    output.toString());
+
+      // invalid resource
+      resource = new FileResource("guru", null);
+      assertFalse("writing", resource.write(output));
+
+      m_logger.addExpected("WARNING: cannot obtain input stream for guru");
+      Config.setRewriting(true);
+    }
+
+    //......................................................................
+  }
+
+  //........................................................................
+}
