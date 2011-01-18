fd0e7e78ab57dad7c316ad4aeb1892adfd2d2da0
balsiger@ixitxachitls.net <balsiger@ixitxachitls.net@b9f0664d-d9d2-19a3-23b2-6533f2ddd7f2>
2011-01-18 22:17:37 +0000

    Added Type and AbstractType

---
+++ trunk/java/net/ixitxachitls/dma/entries/BaseType.java
@@ -0,0 +1,124 @@
+/******************************************************************************
+ * Copyright (c) 2002-2007 Peter 'Merlin' Balsiger and Fredy 'Mythos' Dobler
+ * All rights reserved
+ *
+ * This file is part of Dungeon Master Assistant.
+ *
+ * Dungeon Master Assistant is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Dungeon Master Assistant is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Dungeon Master Assistant; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *****************************************************************************/
+
+//------------------------------------------------------------------ imports
+
+package net.ixitxachitls.dma.entries;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.Immutable;
+
+
+//..........................................................................
+
+//------------------------------------------------------------------- header
+
+/**
+ * The type specification for a base entry.
+ *
+ * @file          BaseType.java
+ *
+ * @author        balsiger@ixitxachitls.net (Peter Balsiger)
+ *
+ * @param         <T> the type represented by this type spec
+ *
+ */
+
+//..........................................................................
+
+//__________________________________________________________________________
+
+@Immutable
+public class BaseType<T /* extends BaseEntry */> extends AbstractType<T>
+{
+  //--------------------------------------------------------- constructor(s)
+
+  //------------------------------- BaseType -------------------------------
+
+  /**
+   * Create the type.
+   *
+   * @param       inClass the class represented by this type
+   *
+   */
+  public BaseType(@Nonnull Class<T> inClass)
+  {
+    super(inClass);
+  }
+
+  //........................................................................
+  //------------------------------- BaseType -------------------------------
+
+  /**
+   * Create the type.
+   *
+   * @param       inClass    the class represented by this type
+   * @param       inMultiple the name to use for multiple entries of the type
+   *
+   */
+  public BaseType(@Nonnull Class<T> inClass, @Nonnull String inMultiple)
+  {
+    super(inClass, inMultiple);
+  }
+
+  //........................................................................
+
+  //........................................................................
+
+  //-------------------------------------------------------------- variables
+
+  /** All the non-base types available. */
+  private static final Map<String, BaseType<?/* extends BaseEntry */>> s_types =
+    new HashMap<String, BaseType<?/* extends BaseEntry*/>>();
+
+  //........................................................................
+
+  //-------------------------------------------------------------- accessors
+
+  //------------------------------- getTypes -------------------------------
+
+  /**
+   * Get the non-base types available.
+   *
+   * @return      all the non-base types
+   *
+   */
+  public static @Nonnull
+    Collection<BaseType<?/* extends BaseEntry*/>> getTypes()
+  {
+    return Collections.unmodifiableCollection(s_types.values());
+  }
+
+  //........................................................................
+
+  //........................................................................
+
+  //----------------------------------------------------------- manipulators
+  //........................................................................
+
+  //------------------------------------------------- other member functions
+  //........................................................................
+}
