3c1a5739c60b8c4f4601f4430410e7d156a70cb6
balsiger <balsiger@ixitxachitls.net>
2011-02-21 19:45:33 +0000

    Added Right command

---
+++ java/net/ixitxachitls/output/commands/Right.java
@@ -0,0 +1,122 @@
+/******************************************************************************
+ * Copyright (c) 2002,2003 Peter 'Merlin' Balsiger and Fredy 'Mythos' Dobler
+ * All rights reserved
+ *
+ * This file is part of Dungeon Master Assistant.
+ *
+ * Dungeon Master Assistant is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Dungeon Master Assistant is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Dungeon Master Assistant; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *****************************************************************************/
+
+//------------------------------------------------------------------ imports
+
+package net.ixitxachitls.output.commands;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.Immutable;
+
+import net.ixitxachitls.util.configuration.Config;
+
+//..........................................................................
+
+//------------------------------------------------------------------- header
+
+/**
+ * The right command.
+ *
+ * @file          Right.java
+ *
+ * @author        balsiger@ixitxachitls.net (Peter Balsiger)
+ *
+ */
+
+//..........................................................................
+
+//__________________________________________________________________________
+
+public class Right extends BaseCommand
+{
+  //--------------------------------------------------------- constructor(s)
+
+  //--------------------------------- Right --------------------------------
+
+  /**
+   * The constructor for the right command.
+   *
+   * @param       inText the text to set right
+   *
+   */
+  public Right(Object inText)
+  {
+    this();
+
+    withArguments(inText);
+  }
+
+  //........................................................................
+  //--------------------------------- Right --------------------------------
+
+  /**
+   * This is the internal constructor for a command.
+   *
+   */
+  protected Right()
+  {
+    super(RIGHT, 0, 1);
+  }
+
+  //........................................................................
+
+  //........................................................................
+
+  //-------------------------------------------------------------- variables
+
+  /** Command for right aligned printing. */
+  public static final String RIGHT =
+    Config.get("resource:commands/right", "right");
+
+  //........................................................................
+
+  //-------------------------------------------------------------- accessors
+  //........................................................................
+
+  //----------------------------------------------------------- manipulators
+  //........................................................................
+
+  //------------------------------------------------- other member functions
+  //........................................................................
+
+  //------------------------------------------------------------------- test
+
+  /** The test. */
+  public static class Test extends net.ixitxachitls.util.test.TestCase
+  {
+    //----- arguments ------------------------------------------------------
+
+    /** Testing arguments. */
+    @org.junit.Test
+    public void arguments()
+    {
+      Command command = new Right("text");
+      assertEquals("setup", "\\right{text}", command.toString());
+
+      command = new Right(new Bold("text"));
+      assertEquals("setup", "\\right{\\bold{text}}", command.toString());
+    }
+
+    //......................................................................
+  }
+
+  //........................................................................
+}
