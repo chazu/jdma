3133eab3df7e8b3b7bacbfc3720e457256343706
balsiger@ixitxachitls.net <balsiger@ixitxachitls.net@b9f0664d-d9d2-19a3-23b2-6533f2ddd7f2>
2011-02-23 19:44:58 +0000

    Added Hat

---
+++ trunk/java/net/ixitxachitls/output/commands/Hat.java
@@ -0,0 +1,122 @@
+/******************************************************************************
+ * Copyright (c) 2002-2011 Peter 'Merlin' Balsiger and Fredy 'Mythos' Dobler
+ * All rights reserved
+ *
+ * This file is part of Dungeon Master Assistant.
+ *
+ * Dungeon Master Assistant is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Dungeon Master Assistant is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Dungeon Master Assistant; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *****************************************************************************/
+
+//------------------------------------------------------------------ imports
+
+package net.ixitxachitls.output.commands;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.Immutable;
+
+import net.ixitxachitls.util.configuration.Config;
+
+//..........................................................................
+
+//------------------------------------------------------------------- header
+
+/**
+ * The hat command.
+ *
+ * @file          Hat.java
+ *
+ * @author        balsiger@ixitxachitls.net (Peter Balsiger)
+ *
+ */
+
+//..........................................................................
+
+//__________________________________________________________________________
+
+@Immutable
+public class Hat extends BaseCommand
+{
+  //--------------------------------------------------------- constructor(s)
+
+  //---------------------------------- Hat ---------------------------------
+
+  /**
+   * The constructor for the hat command.
+   *
+   * @param       inText the text to set hat
+   *
+   */
+  public Hat(@Nonnull Object inText)
+  {
+    this();
+
+    withArguments(inText);
+  }
+
+  //........................................................................
+  //---------------------------------- Hat ---------------------------------
+
+  /**
+   * This is the internal constructor for a command.
+   *
+   * @undefined   never
+   *
+   */
+  protected Hat()
+  {
+    super(HAT, 0, 1);
+  }
+
+  //........................................................................
+
+  //........................................................................
+
+  //-------------------------------------------------------------- variables
+
+  /** Command for adding hatted characters. */
+  public static final String HAT =
+    Config.get("resource:commands/hat", "hat");
+
+  //........................................................................
+
+  //-------------------------------------------------------------- accessors
+  //........................................................................
+
+  //----------------------------------------------------------- manipulators
+  //........................................................................
+
+  //------------------------------------------------- other member functions
+  //........................................................................
+
+  //------------------------------------------------------------------- test
+
+  /** The test. */
+  public static class Test extends net.ixitxachitls.util.test.TestCase
+  {
+    //----- arguments ------------------------------------------------------
+
+    /** Testing arguments. */
+    @org.junit.Test
+    public void arguments()
+    {
+      Command command = new Hat("text");
+      assertEquals("setup", "\\hat{text}", command.toString());
+    }
+
+    //......................................................................
+  }
+
+  //........................................................................
+}
