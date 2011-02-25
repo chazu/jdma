f1987c81a1fa822147b7d3608c7fbc5ed7783773
balsiger <balsiger@ixitxachitls.net>
2011-02-25 17:36:41 +0000

    Added WordUpperCase commands and actions

---
+++ java/net/ixitxachitls/output/actions/WordUpperCase.java
@@ -0,0 +1,137 @@
+/******************************************************************************
+ * Copyright (c) 2002,2003 Peter 'Merlin' Balsiger and Fredy 'Mythos' Dobler
+ * All rights reserved
+ *
+ * This file is part of Dungeon Master Assistant.
+ *
+ * Dungeon Master Assistant is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Dungeon Master Assistant is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Dungeon Master Assistant; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *****************************************************************************/
+
+//------------------------------------------------------------------ imports
+
+package net.ixitxachitls.output.actions;
+
+import java.util.List;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.Immutable;
+
+import net.ixitxachitls.output.Document;
+import net.ixitxachitls.util.Encodings;
+
+//..........................................................................
+
+//------------------------------------------------------------------- header
+
+/**
+ * This is a WordUpperCaseding action, allow to replace all white spaces with a
+ * given character.
+ *
+ * @file          WordUpperCase.java
+ *
+ * @author        balsiger@ixitxachitls.net (Peter 'Merlin' Balsiger)
+ *
+ */
+
+//..........................................................................
+
+//__________________________________________________________________________
+
+@Immutable
+public class WordUpperCase extends Action
+{
+  //--------------------------------------------------------- constructor(s)
+
+  //------------------------------ WordUpperCase ---------------------------
+
+  /**
+   * Construct the action.
+   *
+   */
+  public WordUpperCase()
+  {
+    // nothing to do
+  }
+
+  //........................................................................
+
+  //........................................................................
+
+  //-------------------------------------------------------------- variables
+  //........................................................................
+
+  //-------------------------------------------------------------- accessors
+  //........................................................................
+
+  //----------------------------------------------------------- manipulators
+  //........................................................................
+
+  //------------------------------------------------- other member functions
+
+  //------------------------------- execute --------------------------------
+
+  /**
+   * Execute the action onto the given document.
+   *
+   * @param       inDocument  the document to output to
+   * @param       inOptionals the optional argument
+   * @param       inArguments the arguments
+   *
+   */
+  public void execute(@Nonnull Document inDocument,
+                      @Nullable List<? extends Object> inOptionals,
+                      @Nullable List<? extends Object> inArguments)
+  {
+    if(inArguments == null || inArguments.size() != 1)
+      throw new IllegalArgumentException("expecting just one argument");
+
+    inDocument.add(Encodings
+                   .toWordUpperCase(inDocument.convert(inArguments.get(0))));
+  }
+
+  //........................................................................
+
+  //........................................................................
+
+  //------------------------------------------------------------------- test
+
+  /** The test. */
+  public static class Test extends net.ixitxachitls.util.test.TestCase
+  {
+    //----- all ------------------------------------------------------------
+
+    /** Test all. */
+    @org.junit.Test
+    public void all()
+    {
+      Action action = new WordUpperCase();
+
+      Document doc = new Document();
+
+      action.execute(doc, null,
+                     com.google.common.collect.ImmutableList.of
+                     ("this is the text to make upper case"));
+
+      assertEquals("execution did not produce desired result",
+                   "This Is The Text To Make Upper Case",
+                   doc.toString());
+    }
+
+    //......................................................................
+  }
+
+  //........................................................................
+}
