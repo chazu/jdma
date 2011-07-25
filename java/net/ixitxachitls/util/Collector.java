00f4512edf76c73c94a44af5e318b9fd162e4cab
balsiger <balsiger@ixitxachitls.net>
2011-07-25 16:24:18 +0000

    Remove some unused files

---
+++ java/net/ixitxachitls/util/Collector.java
@@ -0,0 +1,76 @@
+/******************************************************************************
+ * Copyright (c) 2002-2011 Peter 'Merlin' Balsiger and Fredy 'Mythos' Dobler
+ * All rights reserved
+ *
+ * This file is part of Dungeon Master Assistant.
+ *
+ * Dungeon Master Assistant is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Dungeon Master Assistant is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Dungeon Master Assistant; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *****************************************************************************/
+
+//------------------------------------------------------------------ imports
+
+package net.ixitxachitls.util;
+
+import java.io.Serializable;
+
+import javax.annotation.Nonnull;
+
+//..........................................................................
+
+//------------------------------------------------------------------- header
+
+/**
+ * An interface to extract data from an object.
+ *
+ * @file          Collector.java
+ *
+ * @author        balsiger@ixitxachitls.net (Peter Balsiger)
+ *
+ * @param         <T> The type to extract from.
+ * @param         <V> The type extracted.
+ *
+ */
+
+//..........................................................................
+
+//__________________________________________________________________________
+
+public interface Collector<T, V> extends Serializable
+{
+  //-------------------------------------------------------------- accessors
+
+  //------------------------------- collect --------------------------------
+
+  /**
+   * Get the value from the given object.
+   *
+   * @param       ioCollected the data collected so far
+   * @param       inObject    the object to get the data from
+   *
+   * @return      the collected data
+   *
+   */
+  public @Nonnull V collect(@Nonnull V ioCollected, @Nonnull T inObject);
+
+  //........................................................................
+
+  //........................................................................
+
+  //----------------------------------------------------------- manipulators
+  //........................................................................
+
+  //------------------------------------------------- other member functions
+  //........................................................................
+}
