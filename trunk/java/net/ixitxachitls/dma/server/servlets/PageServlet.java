44bac306c2949e8f4cc81bf1beb185b760bde842
balsiger@ixitxachitls.net <balsiger@ixitxachitls.net@b9f0664d-d9d2-19a3-23b2-6533f2ddd7f2>
2012-02-01 15:17:29 +0000

    fixed tests

--- trunk/java/net/ixitxachitls/dma/server/servlets/PageServlet.java
+++ trunk/java/net/ixitxachitls/dma/server/servlets/PageServlet.java
@@ -662,6 +662,9 @@ public class PageServlet extends DMAServlet
                    + "alt=\"Valid XHTML 1.0!\" /></a>\n"
                    + "      <IMG src=\"/icons/html5.png\" "
                    + "alt=\"Uses HTML 5!\"/>\n"
+                   + "      <IMG src=\"http://code.google.com/appengine/"
+                   + "images/appengine-silver-120x30.gif\" "
+                   + "alt=\"Powered by Google App Engine\"/>\n"
                    + "    </DIV>\n"
                    + "  </BODY>\n"
                    + "</HTML>\n",
