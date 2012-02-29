a2bbdc8d2abbeaa8b80e914ef25e07d23753d673
balsiger@ixitxachitls.net <balsiger@ixitxachitls.net@b9f0664d-d9d2-19a3-23b2-6533f2ddd7f2>
2012-02-29 14:21:29 +0000

    Added Incomplete extension

---
+++ trunk/java/net/ixitxachitls/dma/entries/extensions/Incomplete.java
@@ -0,0 +1,152 @@
+/******************************************************************************
+ * Copyright (c) 2002-2012 Peter 'Merlin' Balsiger and Fred 'Mythos' Dobler
+ * All rights reserved
+ *
+ * This file is part of Dungeon Master Assistant.
+ *
+ * Dungeon Master Assistant is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Dungeon Master Assistant is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Dungeon Master Assistant; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *****************************************************************************/
+
+//------------------------------------------------------------------ imports
+
+package net.ixitxachitls.dma.entries.extensions;
+
+import javax.annotation.Nonnull;
+
+import net.ixitxachitls.dma.entries.Entry;
+import net.ixitxachitls.dma.values.Text;
+import net.ixitxachitls.dma.output.ListPrint;
+import net.ixitxachitls.dma.output.Print;
+
+//..........................................................................
+
+//------------------------------------------------------------------- header
+
+/**
+ * This is the timed extension for all the entries.
+ *
+ * @file          Incomplete.java
+ *
+ * @author        balsiger@ixitxachitls.net (Peter 'Merlin' Balsiger)
+ *
+ */
+
+//..........................................................................
+
+//__________________________________________________________________________
+
+public class Incomplete extends Extension<Entry<?>>
+{
+  //--------------------------------------------------------- constructor(s)
+
+  //------------------------------- Incomplete -----------------------------
+
+  /**
+   * Default constructor.
+   *
+   * @param       inEntry the entry attached to
+   * @param       inName the name of the extension
+   *
+   */
+  public Incomplete(@Nonnull Entry inEntry, @Nonnull String inName)
+  {
+    super(inEntry, inName);
+  }
+
+  //........................................................................
+  //------------------------------- Incomplete -----------------------------
+
+  /**
+   * Default constructor.
+   *
+   * @param       inEntry the entry attached to
+   * @param       inTag   the tag name for this instance
+   * @param       inName  the name of the extension
+   *
+   */
+  // public Incomplete(Entry inEntry, String inTag, String inName)
+  // {
+  //   super(inEntry, inTag, inName);
+  // }
+
+  //........................................................................
+
+  //........................................................................
+
+  //-------------------------------------------------------------- variables
+
+  //----- incomplete -------------------------------------------------------
+
+  /** The time that is left for the item. */
+  @Key("incomplete")
+  @DM
+  protected @Nonnull Text m_incomplete = new Text();
+
+  //........................................................................
+
+  static
+  {
+    extractVariables(Incomplete.class);
+  }
+
+  //........................................................................
+
+  //-------------------------------------------------------------- accessors
+
+  //----------------------------- getPagePrint -----------------------------
+
+  /**
+   * Get the print for a full page.
+   *
+   * @return the print for page printing
+   *
+   */
+  @Override
+  protected @Nonnull Print getPagePrint()
+  {
+    return s_pagePrint;
+  }
+
+  //........................................................................
+  //----------------------------- getListPrint -----------------------------
+
+  /**
+   * Get the print for a list entry.
+   *
+   * @return the print for list entry
+   *
+   */
+  @Override
+  protected @Nonnull ListPrint getListPrint()
+  {
+    return s_listPrint;
+  }
+
+  //........................................................................
+
+  //........................................................................
+
+  //----------------------------------------------------------- manipulators
+  //........................................................................
+
+  //------------------------------------------------- other member functions
+  //........................................................................
+
+  //------------------------------------------------------------------- test
+
+  // no tests, see BaseItem for tests
+
+  //........................................................................
+}
